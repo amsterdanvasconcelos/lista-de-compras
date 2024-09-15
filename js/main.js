9819f025525bc3677ba23d9d28fe6b86be4bffef
Amsterdan Vasconcelos <amsterdan.sci@gmail.com>
2024-09-15 13:48:07 -0300

    feat: add toast notification

--- js/main.js
+++ js/main.js
@@ -8,21 +8,39 @@ import {
 
 const formAdd = document.querySelector('[data-js="add"]');
 const productsList = document.querySelector('[data-js="products-list"]');
+const notifications = document.querySelector('[data-js="notifications"]');
+
+let timeOutId = null;
+
+const showAlert = (message) => {
+  const html = `
+    <div class="toast">
+      <div class="toast__content">
+        <p class="toast__title">Error:</p>
+        <p class="toast__message">${message}</p>
+      </div>
+    </div>
+  `;
+
+  notifications.innerHTML = html;
+
+  if (timeOutId) clearTimeout(timeOutId);
+  timeOutId = setTimeout(() => (notifications.innerHTML = ''), 5300);
+};
 
 const validateProductName = (productName) => {
   if (!productName.length) {
-    alert('Você não disse qual produto adicionar!');
+    showAlert('Você não disse qual produto adicionar!');
     return false;
   }
 
   const products = productsList.querySelectorAll('li');
-
   const alreadyExists = Array.from(products).some(
     (product) => product.dataset.productname === productName
   );
 
   if (alreadyExists) {
-    alert('Este produto já existe!');
+    showAlert('Este produto já existe!');
     return false;
   }
 
